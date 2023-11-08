313949f062b12b06d990de92a6522abd6a59eebb
Tim <141640156+TStokes06@users.noreply.github.com>
2023-11-08 23:09:53 +0000

    all

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import './SecondsCounter.css';
+
+const SecondsCounter = ({ initialSeconds }) => {
+  // ... (rest of the code remains the same)
+};
+
+export default SecondsCounter;
